04a34cc2e9f8f0e066e885c5599ef54c95694574
henry komatsu <komatsuhenry@gmail.com>
2026-04-06 10:50:46 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // configuracao de rotas
+    // { path: 'home', component: HomeComponent },
+    
+];
